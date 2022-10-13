f32c9b2d68e9fca2b7b1a292bff4babd386ba12c
adricosmicfox <adriiphone@gmail.com>
2022-10-13 18:11:27 +0200

    snack 3 completed

--- snack 3/index.php
+++ snack 3/index.php
@@ -1,3 +1,59 @@
 <!-- Snack 3
 Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post.
 Qui l’array di esempio: https://www.codepile.net/pile/R2K5d68z -->
+
+
+<?php
+
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+// nuova variabile con valore (tramite funzione) chiavi array posts
+$keys = array_keys($posts);
+// cicliamo l'array posts
+for ($i = 0; $i < count($posts); $i++) {
+    echo $keys[$i] . "<br>";
+
+    foreach ($posts[$keys[$i]] as $key => $value) {
+        echo $value['author'] . " : " . $value['title'] . " : " . $value['text'] . "<br>";
+    }
+    echo "<br>";
+}
+
+?>
